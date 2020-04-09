8a4d85bb4a8a52478d866555a0d3652ba29adf02
Giorgio Torres <giorgio.torres@sumup.com>
2020-04-09 15:29:36 -0300

    First commit

---
+++ lib/jenkiexs.ex
@@ -0,0 +1,18 @@
+defmodule Jenkiexs do
+  @moduledoc """
+  Documentation for Jenkiexs.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Jenkiexs.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/jenkiexs_test.exs
@@ -0,0 +1,8 @@
+defmodule JenkiexsTest do
+  use ExUnit.Case
+  doctest Jenkiexs
+
+  test "greets the world" do
+    assert Jenkiexs.hello() == :world
+  end
+end
