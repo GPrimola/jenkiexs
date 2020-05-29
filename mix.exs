80b0f7f0efacba6a5bb513df4b1cc124f7cc079a
Giorgio Torres <torres.giorgio@gmail.com>
2020-05-29 20:00:35 -0300

    Changed mix.exs to be published

--- mix.exs
+++ mix.exs
@@ -4,10 +4,13 @@ defmodule Jenkiexs.MixProject do
   def project do
     [
       app: :jenkiexs,
-      version: "0.1.0",
+      version: "0.9.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      package: package(),
+      deps: deps(),
+      source_url: "https://github.com/GPrimola/jenkiexs",
+      homepage_url: "https://github.com/GPrimola/jenkiexs"
     ]
   end
 
@@ -24,7 +27,19 @@ defmodule Jenkiexs.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:httpoison, "~> 1.6"},
-      {:jason, "~> 1.2"}
+      {:jason, "~> 1.2"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  def package do
+    [
+      name: "jenkiexs",
+      description: """
+      This is a [Jenkins](https://www.jenkins.io/) client written in Elixir.
+      """,
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/GPrimola/jenkiexs"}
     ]
   end
 end
