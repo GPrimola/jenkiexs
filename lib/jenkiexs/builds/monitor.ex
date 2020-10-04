27aabc9c8d7f5782798b6447de5f578ca9e4b263
Giorgio Torres <torres.giorgio@gmail.com>
2020-10-04 09:41:19 -0300

    Docs for Jenkiexs.Builds.Monitor.monitor/1

--- lib/jenkiexs/builds/monitor.ex
+++ lib/jenkiexs/builds/monitor.ex
@@ -5,23 +5,39 @@ defmodule Jenkiexs.Builds.Monitor do
   @queue_time 2_000
   @after_build_duration_time 1_000
 
+  @doc """
+  Monitors when a running build ends.
+
+  Returns a task that yields a build when complete.
+
+  ## Examples
+
+    iex> Jenkiexs.Builds.Monitor.monitor(%Build{})
+    {:ok, task}
+
+    iex> Jenkiexs.Builds.Monitor.monitor(%Build{})
+    {:ok, Build.t()}
+  """
+
   @spec monitor(Build.t()) :: {:ok, Task.t()}
   def monitor(%Build{estimated_duration: duration} = build) do
-    task = Task.async(fn ->
-      check(build, duration + @queue_time)
-    end)
+    task =
+      Task.async(fn ->
+        check(build, duration + @queue_time)
+      end)
 
     {:ok, task}
   end
 
   defp check(build, timeout) do
     Process.sleep(timeout)
+
     case Builds.details(build) do
       {:ok, %{building?: false} = completed_build} ->
         {:ok, completed_build}
+
       _ ->
         check(build, @after_build_duration_time)
     end
   end
-
 end
---
+++ test/jenkiexs/builds/monitor_test.exs
@@ -0,0 +1,6 @@
+defmodule Jenkiexs.Builds.MonitorTest do
+  use ExUnit.Case, async: true
+  alias Jenkiexs.Builds.Monitor
+
+  doctest Monitor
+end
