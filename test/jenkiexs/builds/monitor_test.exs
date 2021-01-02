e7525ed62b935bff4affbcfb4d9d7dde0d9b1d40
Giorgio Torres <torres.giorgio@gmail.com>
2021-01-02 17:46:49 -0300

    Added tests for Jenkiexs.Builds.Monitor.monitor/1

--- test/jenkiexs/builds/monitor_test.exs
+++ test/jenkiexs/builds/monitor_test.exs
@@ -1,3 +1,20 @@
 defmodule Jenkiexs.Builds.MonitorTest do
   use ExUnit.Case, async: true
+
+  alias Jenkiexs.Builds.{Build, Monitor}
+
+  describe "monitor/1" do
+    test "should return tuple {:ok, task} when a build is given" do
+      build = %Build{job_name: "Test"}
+      assert {:ok, task} = Monitor.monitor(build)
+      assert %Task{} = task
+    end
+
+    test "should return a task alive when build is building" do
+      build = %Build{job_name: "Test", building?: true}
+      assert {:ok, task} = Monitor.monitor(build)
+      assert %Task{} = task
+      assert Process.alive?(task.pid)
+    end
+  end
 end
